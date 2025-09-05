cc0886abcf245276a8a498d70c987db508f76a4f
Aurora <auduong.301@gmail.com>
2025-09-05 18:09:51 +0700

    update

---
+++ app/layout.js
@@ -0,0 +1,35 @@
+import "@/app/_styles/globals.css";
+
+import Header from "@/app/_components/Header";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josein = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josein.className} text-primary-100 bg-primary-950 rela flex min-h-screen flex-col`}
+      >
+        <Header />
+
+        <div className="grid flex-1 px-8 py-12">
+          <main className="mx-auto w-full max-w-7xl">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ starter/pages/home-page.js
@@ -0,0 +1,19 @@
+export default function Page() {
+  return (
+    <main className="mt-24">
+      <img src="/bg.png" alt="Mountains and forests with two cabins" />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-primary-50 mb-10 text-8xl font-normal tracking-tight">
+          Welcome to paradise.
+        </h1>
+        <a
+          href="/cabins"
+          className="bg-accent-500 text-primary-800 hover:bg-accent-600 px-8 py-6 text-lg font-semibold transition-all"
+        >
+          Explore luxury cabins
+        </a>
+      </div>
+    </main>
+  );
+}
